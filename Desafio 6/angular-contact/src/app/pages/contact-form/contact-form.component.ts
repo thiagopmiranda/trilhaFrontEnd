03b23e444206908745771b18f08b4fead9c7459b
Thiago Miranda <thiagopereira99@gmail.com>
2022-01-19 18:30:06 -0300

    Desafio 6 Revisado

--- Desafio 6/angular-contact/src/app/pages/contact-form/contact-form.component.ts
+++ Desafio 6/angular-contact/src/app/pages/contact-form/contact-form.component.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ContactRequestDto, ContactResponseDto } from './contact';
 import { ContactService } from './contact.service';
 
@@ -10,49 +15,39 @@ import { ContactService } from './contact.service';
   styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
-  formContact: FormGroup;
+  formContact!: FormGroup;
 
-  mensagens = {
-    name: [
-      { tipo: 'required', mensagem: 'Nome é obrigatório!' },
-      {
-        tipo: 'minlenght',
-        mensagem: 'O nome deve ter pelo menos 3 caracteres',
-      },
-    ],
-    email: [
-      { tipo: 'required', mensagem: 'Email é obrigatório!' },
-      { tipo: 'email', mensagem: 'Email Inválido' },
-    ],
-    subject: [{ tipo: 'required', mensagem: 'O campo Subject é obrigatório!' }],
-    message: [
-      { tipo: 'required', mensagem: 'O campo Message é obrigatório!' },
-      {
-        tipo: 'minlenght',
-        mensagem: 'Sua mensagem deve ter pelo menos 3 caracteres',
-      },
-    ],
-  };
+  constructor(private contactService: ContactService) {}
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private contactService: ContactService
-  ) {
-    this.formContact = this.formBuilder.group({
-      name: [
-        '',
-        Validators.compose([Validators.required, Validators.minLength(3)]),
-      ],
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      subject: ['', Validators.required],
-      message: [
-        '',
-        Validators.compose([Validators.required, Validators.minLength(10)]),
-      ],
+  ngOnInit() {
+    this.formContact = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      subject: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      message: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(50),
+      ]),
     });
   }
 
-  ngOnInit() {}
+  get name() {
+    return this.formContact.get('name');
+  }
+
+  get email() {
+    return this.formContact.get('email');
+  }
+  get subject() {
+    return this.formContact.get('subject');
+  }
+  get message() {
+    return this.formContact.get('message');
+  }
 
   postData() {
     if (!this.formContact.valid) {
